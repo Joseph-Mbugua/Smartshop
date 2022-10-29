493e6677b05622f8f958735efdc9aca9c2693e61
Royce Bob <111703033+Royce-B@users.noreply.github.com>
2022-10-29 14:32:08 +0300

    🛍️ 𝚂𝙼𝙰𝚁𝚃𝚂𝙷𝙾𝙿 🛒

---
+++ brain/sequelize.js
@@ -0,0 +1,17 @@
+// 🛍️ 𝚂𝙼𝙰𝚁𝚃𝚂𝙷𝙾𝙿 🛒
+let user = {
+name: "",
+username: "",
+email: "",
+password: ""
+}
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize({
+  user: {},
+  dialect: 'sqlite',
+  storage: 'path/to/brain/smartshop.sqlite'
+});
+
+
+/* Do not edit damn shit 
+if you are not good in js */
